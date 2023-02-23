a66d3dea386b1d0c8874104005566ac1cf084efb
Ivan Kosilov <idkosilov@gmail.com>
2023-02-23 09:00:45 +0700

    Added key value console

---
+++ src/raftkv_console/key_value_console.py
@@ -0,0 +1,71 @@
+import cmd
+from os import PathLike
+
+from raftkv.key_value_storage import KeyValueStorage
+
+
+class RaftKeyValueStorageShell(cmd.Cmd):
+    intro = "Welcome to the raft key-value storage shell. Type help or ? to list commands.\n"
+    prompt = "rkvs> "
+
+    def __init__(self, address: str, cluster: list[str], db_file: str | PathLike) -> None:
+        super().__init__()
+        self.address = address
+        self.cluster = cluster
+        self.db = KeyValueStorage(db_file)
+
+    def do_set(self, arg: str) -> None:
+        """
+        Set a key-value pair in the store.
+
+        Usage: set <key> <value>
+        """
+        key, value = arg.split()
+
+        with self.db:
+            self.db[key] = value
+
+    def do_get(self, arg):
+        """
+        Get the value for a given key from the store.
+
+        Usage: get <key>
+        """
+        with self.db:
+            value = self.db.get(arg)
+
+        if value is not None:
+            print(value)
+        else:
+            print("Key not found")
+
+    def do_delete(self, arg):
+        """
+        Delete a key-value pair from the store.
+
+        Usage: delete <key>
+        """
+        with self.db:
+            if arg in self.db:
+                del self.db[arg]
+            else:
+                print("Key not found")
+
+    def do_list(self):
+        """
+        List all keys in the store.
+
+        Usage: list
+        """
+        with self.db:
+            for key in self.db:
+                print(key)
+
+    @staticmethod
+    def do_quit():
+        """
+        Quit the shell.
+
+        Usage: quit
+        """
+        return True
