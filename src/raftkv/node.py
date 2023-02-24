6b07528510ce008196c1bae1c62b62952888e4e9
Ivan Kosilov <idkosilov@gmail.com>
2023-02-25 05:19:49 +0700

    Added on vote response implementation

--- src/raftkv/node.py
+++ src/raftkv/node.py
@@ -1,6 +1,7 @@
+from math import ceil
 from typing import Callable, Optional, Any
 
-from raftkv.messages import Message, VoteRequest
+from raftkv.messages import Message, VoteRequest, VoteResponse
 from raftkv.state import AbstractState, Role
 
 
@@ -38,12 +39,69 @@ class Node:
 
         vote_request = VoteRequest(term=self.state.current_term,
                                    candidate_id=self.node_id,
-                                   last_log_index=len(self.state.log),
+                                   last_log_index=len(self.state.log) - 1,
                                    last_log_term=self.state.last_log_term)
 
-        recipients_vote_request = self.nodes_ids.difference((self.node_id, ))
+        recipients_vote_request = self.nodes_ids.difference((self.node_id,))
 
         for node_id in recipients_vote_request:
             self.send_message_callback(node_id, vote_request)
 
         self.start_election_timer_callback()
+
+    def on_vote_request(self, vote_request: VoteRequest) -> None:
+        """
+        Handles a received vote request from another node.
+
+        :param vote_request: the vote request received from another node.
+        """
+        if vote_request.term > self.state.current_term:
+            self.state.current_term = vote_request.term
+            self.state.current_role = Role.FOLLOWER
+            self.state.voted_for = None
+
+        is_term_ok = vote_request.term == self.state.current_term
+        is_log_up_to_date = vote_request.last_log_term > self.state.last_log_term or (
+                vote_request.last_log_term == self.state.last_log_term and
+                vote_request.last_log_index >= self.state.last_log_index)
+        already_voted_for_this_candidate_or_did_not_voted = self.state.voted_for in (vote_request.candidate_id, None)
+
+        if is_term_ok and is_log_up_to_date and already_voted_for_this_candidate_or_did_not_voted:
+            self.state.voted_for = vote_request.candidate_id
+            message = VoteResponse(term=self.state.current_term,
+                                   node_id=self.node_id,
+                                   granted=True)
+        else:
+            message = VoteResponse(term=self.state.current_term,
+                                   node_id=self.node_id,
+                                   granted=False)
+
+        self.send_message_callback(vote_request.candidate_id, message)
+
+    def on_vote_response(self, vote_response: VoteResponse) -> None:
+        """
+        Handles a received vote response from another node.
+
+        :param vote_response: the vote response received from another node.
+        """
+        if self.state.current_role == Role.CANDIDATE and \
+                self.state.current_term == vote_response.term and vote_response.granted:
+            self.state.votes_received.add(vote_response.node_id)
+            if len(self.state.votes_received) >= ceil((len(self.nodes_ids) + 1) / 2):
+                self.state.current_role = Role.LEADER
+                self.state.current_leader = self.node_id
+                self.cancel_election_timer_callback()
+                followers_ids = self.nodes_ids.difference((self.node_id,))
+                for follower_id in followers_ids:
+                    self.state.next_index[follower_id] = len(self.state.log)
+                    self.state.match_index[follower_id] = 0
+                    self.replicate_log(follower_id)
+        elif vote_response.term > self.state.current_term:
+            self.state.current_term = vote_response.term
+            self.state.current_role = Role.FOLLOWER
+            self.state.voted_for = None
+            self.cancel_election_timer_callback()
+
+    def replicate_log(self, follower_id: int) -> None:
+        ...
+        # TODO
