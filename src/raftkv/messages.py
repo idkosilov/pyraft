0c7778584b5cbc7f7bbfcd13863c282b806e3c66
Ivan Kosilov <idkosilov@gmail.com>
2023-02-25 05:20:32 +0700

    Added AppendEntriesRequest and AppendEntriesResponse messages

--- src/raftkv/messages.py
+++ src/raftkv/messages.py
@@ -1,6 +1,8 @@
 from abc import ABC
 from dataclasses import dataclass
 
+from raftkv.state import Entry
+
 
 class Message(ABC):
     ...
@@ -19,3 +21,21 @@ class VoteResponse(Message):
     term: int
     node_id: int
     granted: bool
+
+
+@dataclass
+class AppendEntriesRequest(Message):
+    term: int
+    leader_id: int
+    previous_log_index: int
+    previous_log_term: int
+    entries: list[Entry]
+    leader_commit: int
+
+
+@dataclass
+class AppendEntriesResponse(Message):
+    term: int
+    node_id: int
+    ack: int
+    success: bool
