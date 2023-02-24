439099814de04f07aec6da16f227980e52b6aab0
Ivan Kosilov <idkosilov@gmail.com>
2023-02-25 01:35:57 +0700

    Added VoteRequest message

--- src/raftkv/messages.py
+++ src/raftkv/messages.py
@@ -1,5 +1,14 @@
 from abc import ABC
+from dataclasses import dataclass
 
 
 class Message(ABC):
     ...
+
+
+@dataclass
+class VoteRequest(Message):
+    term: int
+    candidate_id: int
+    last_log_index: int
+    last_log_term: int
