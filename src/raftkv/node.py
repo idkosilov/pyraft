cfc1d8a75f40397ac6269eced125774d0de27dc6
Ivan Kosilov <idkosilov@gmail.com>
2023-02-25 01:36:24 +0700

    Added implementation of on_election_timeout_or_leader_fault

--- src/raftkv/node.py
+++ src/raftkv/node.py
@@ -1,7 +1,7 @@
 from typing import Callable, Optional, Any
 
-from raftkv.messages import Message
-from raftkv.state import AbstractState
+from raftkv.messages import Message, VoteRequest
+from raftkv.state import AbstractState, Role
 
 
 class Node:
@@ -9,28 +9,41 @@ class Node:
     A class representing a node in a Raft cluster.
     """
 
-    def __init__(self, node_id: int, nodes: set[int], state: AbstractState) -> None:
+    def __init__(self, node_id: int, nodes_ids: set[int], state: AbstractState) -> None:
         """
         Initializes a new instance of the Node class.
 
         :param node_id: the unique ID of the node.
-        :param nodes: a set of IDs of all nodes in the cluster.
+        :param nodes_ids: a set of IDs of all nodes in the cluster.
         :param state: the state object containing the node's current state.
         """
         self.node_id = node_id
-        self.nodes_ids = nodes
+        self.nodes_ids = nodes_ids
         self.state = state
 
-        # a callback function that sends a message to another node.
         self.send_message_callback: Optional[Callable[[int, Message], None]] = None
-
-        # a callback function that deliver changes to the app.
         self.deliver_changes_callback: Optional[Callable[[Any], None]] = None
-
-        # a callback function that cancels the node's election timer.
         self.cancel_election_timer_callback: Optional[Callable[[], None]] = None
-
-        # a callback function that starts the node's election timer.
         self.start_election_timer_callback: Optional[Callable[[], None]] = None
 
+    def on_election_timeout_or_leader_fault(self) -> None:
+        """
+        The function to be called when an election timeout or leader fault occurs.
+        Initiates a new election and sends VoteRequest messages to all other nodes in the cluster.
+        """
+        self.state.current_term += 1
+        self.state.current_role = Role.CANDIDATE
+        self.state.voted_for = self.node_id
+        self.state.votes_received = {self.node_id, }
 
+        vote_request = VoteRequest(term=self.state.current_term,
+                                   candidate_id=self.node_id,
+                                   last_log_index=len(self.state.log),
+                                   last_log_term=self.state.last_log_term)
+
+        recipients_vote_request = self.nodes_ids.difference((self.node_id, ))
+
+        for node_id in recipients_vote_request:
+            self.send_message_callback(node_id, vote_request)
+
+        self.start_election_timer_callback()
