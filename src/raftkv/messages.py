ce84442d5e1a6cd133e074d2de382a892298eee8
Ivan Kosilov <idkosilov@gmail.com>
2023-02-25 02:30:08 +0700

    Added VoteResponse message

--- src/raftkv/messages.py
+++ src/raftkv/messages.py
@@ -12,3 +12,10 @@ class VoteRequest(Message):
     candidate_id: int
     last_log_index: int
     last_log_term: int
+
+
+@dataclass
+class VoteResponse(Message):
+    term: int
+    node_id: int
+    granted: bool
