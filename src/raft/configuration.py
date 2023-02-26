bec931782e3addea05e271fc60a0387061ba23cd
Ivan Kosilov <idkosilov@gmail.com>
2023-02-27 01:03:38 +0700

    Added RaftConfiguration

--- src/raft/configuration.py
+++ src/raft/configuration.py
@@ -1,8 +1,25 @@
 from dataclasses import dataclass
+from os import PathLike
 
 
 @dataclass
 class NodeConfiguration:
     node_id: int
+
+
+@dataclass
+class ZmqNodeConfiguration(NodeConfiguration):
     host: str
     port: int
+
+    def url(self) -> str:
+        return f"tcp://{self.host}:{self.port}"
+
+
+class RaftConfiguration:
+    node_id: int
+    storage_path: str | PathLike
+    heartbeat_timeout: int
+    election_timeout_lower: int
+    election_timeout_upper: int
+    cluster: set[NodeConfiguration]
